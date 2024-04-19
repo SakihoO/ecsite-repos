5a64c1bcb56d427f7da060cf2eb369d8cf6bab20
SakihoO <s.okamoto@internous.co.jp>
2024-04-19 15:42:06 +0900

    index.jsの不要なコメントアウトを削除

--- frontend/index.js
+++ frontend/index.js
@@ -2,13 +2,5 @@
 const express = require('express');
 const app = express();
 
-// require('');の中身を、mysqlからmysql2に変更
-// const mysql =require('mysql2');
-
-// // ホームページに対してGETリクエストが行われた時に「Hello World!」で応答する
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
 // ポート3000をオープンにする
 app.listen(3000);
